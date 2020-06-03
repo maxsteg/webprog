b9978b0efe448fe46735ec449f97bb3ebfa1f498
KarloSlot <k.h.r.slot@student.rug.nl>
2020-06-03 13:53:42 +0200

    Modify Functions and PHP

--- index.php
+++ index.php
@@ -6,28 +6,21 @@ include __DIR__ . '/tpl/body_start.php';
 
 <div class="row">
     <div class="col-md-12" style="background-color: darkgray;">
-        <form action="index.php" method="POST">
-            <?php
-            if (isset($_POST['joinGame'])) {
-                echo '<div class="form-group"><label>Game Code</label><input id="gameCode" class="form-control text-uppercase" name="gameCode" type="text" placeholder="Enter a game code..."></div>';
-                echo '<button type="submit" name="joinGameCode" class="btn btn-primary">Join</button>';
-            } else if (isset($_POST['newGame'])) {
-                echo '<h1>Game Code</h1>';
-            }
-
-            if ((isset($_POST['joinGameCode'])) && (isset($_POST['gameCode']))) {
-                echo '<h1>' . $_POST['gameCode'] . '</h1>';
-            }
-
-            ?>
-            <div class="form-group" id="startGameButtons"
-                <?php
-                if ((isset($_POST['newGame'])) || (isset($_POST['joinGame'])) || (isset($_POST['gameCode']))) {
-                    echo 'style="display:none;"'; }
-                ?>
-            >
-                <button type="submit" name="newGame" id="newGame" class="btn btn-primary">New Game</button>
-                <button type="submit" name="joinGame" id="joinGame" class="btn btn-secondary">Join Game</button>
+        <form id="gameCodeForm" action="index.php" method="POST">
+            <div id="enterCode" style="display:none;">
+                <div class="form-group">
+                    <label>Game Code</label>
+                    <input id="gameCode" class="form-control text-uppercase" name="gameCode" type="text" placeholder="e.g. ABC123">
+                    <div class="valid-feedback">
+                        <b>Great!</b> This field looks good!</div>
+                    <div class="invalid-feedback">
+                        <b>Uh oh!</b> This field should contain 6 characters (capital letters or numbers)!</div>
+                </div>
+                <button type="submit" id="submitGameCode" name="submitGameCode" class="btn btn-primary">Join</button>
+            </div>
+            <div class="form-group" id="startGameButtons">
+                <button name="newGame" id="newGame" class="btn btn-primary">New Game</button>
+                <button name="joinGame" id="joinGame" class="btn btn-secondary">Join Game</button>
             </div>
         </form>
     </div>
