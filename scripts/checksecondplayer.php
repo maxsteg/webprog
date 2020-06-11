c6a08da7c425b892660142e9d2a6601193242bc6
rina-d <t.r.donker@student.rug.nl>
2020-06-11 11:41:51 +0200

    Modify checksecondplayer.php, game.js and purple boxes.

--- scripts/checksecondplayer.php
+++ scripts/checksecondplayer.php
@@ -1,3 +1,12 @@
 <?php
+$json_file = file_get_contents("../data/games.json");
+$games = json_decode($json_file, true);
+$gamenumber = $_POST['gamenumber'];
 
 // Check if in games.json with your gamecode a variable exist with player1_time and player2_time
+if (isset($games[$gamenumber]['player1_time']) and isset($games[$gamenumber]['player2_time'])) {
+    echo 'true';
+}
+else {
+    echo 'false';
+}
--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,11 @@
 function checkSecondPlayer() {
-    $.post("scripts/checksecondplayer.php", {gamenumber: gameNumber});
+    let checkSecondPlayer = $.post("scripts/checksecondplayer.php", {gamenumber: gameNumber});
+    let response = checkSecondPlayer.done(function (data) {
+        if (data === 'true') {
+          return true;
+        }
+    })
+    return response;
 }
 
 function checkPresence() {
@@ -7,12 +13,11 @@ function checkPresence() {
     }
 
 function checkOthersPresence() {
-    $.post("scripts/otherspresence.php", {gamenumber: gameNumber, playernumber: playerNumber});
-//     test.done(function (data) {
-//         console.log(data.presence);
-//     });
+    let test2 = $.post("scripts/otherspresence.php", {gamenumber: gameNumber, playernumber: playerNumber});
+    test2.done(function (data) {
+        console.log(data);
+    })
 }
-
 
 
 function openBox() {
@@ -27,57 +32,96 @@ function explodeBomb() {
     // slideIn buttons
 }
 
-
-
 $(function() {
-    let startGame = false;
+    let startGame = true;
 
-    while (startGame === false) {
+    window.setInterval( function () {
         if (checkSecondPlayer()) {
             startGame = true;
+            clearInterval();
         }
-    }
+    }, 3000);
 
+    var yourTurn = true;
 
-    if (startGame) {
-        //    }
-        window.setInterval(function (){
-            checkPresence();
+    window.setInterval(function () {
+        if (startGame) {
+            console.log('hoi');
+            yourTurn = yourTurn();
             if (checkOthersPresence()) {
                 // end game
                 // deletegame
-            }
-
-            if (yourTurn()) {
-                // check if bomb is intact:
-                    // interface veranderen --> JIJ BENT AAN DE BEURT YAY
-                    $('img').on('click', function() {
-                        // krijg het id
-                        // openBox(id)
-                        // check of het nog gesloten, zo niet doe je niks
-                        // open box --> return of het normale box is, hint of bom
-                        // change properties of box (close == false)
-                        // maak box grijs
-
-                        // if box = bom:
-                            // explodeBomb(loser)
-                        // call php function --> endgame.php
-                        // if box = hint:
-                        // pop-up met de hint
-                        // aanpassen in json dat het niet meer jouw beurt is
-                })
-                // else (bom niet intact --> jij hebt gewonnen! : )
-                    // explodeBomb(winner)
-                    // deletegame()
-
-
-            } else {
-                // Niet jouw turn
-                // interface veranderen --> JIJ BENT NIET AAN DE BEURT NAY
+                // }
             }
         }
+
+        $('img').unbind().on('click', function (e) {
+            if (startGame && yourTurn()) {
+                // checkPresence();
+                // if (checkOthersPresence()) {
+                //     // end game
+                //     // deletegame
+                // }
+                // }, 3000);
+                console.log('test');
+                if (openBox() == "opened"):
+                // IS AL GEOPEND
+                else if (openBox() == "empty"):
+                changeTurn();
+            else if (openBox() == "bomb"):
+                explodeBomb("loser");
+                changeTurn();
+            else if (openBox() == "hint"):
+                showHint();
+                changeTurn();
             }
-        , 3000)
+        });
+
+    }, 3000);
 
 
+    // window.setInterval(function() {
+    //
+    // }, 3000)
+
+
+    // if (yourTurn()) {
+    // Check last_opened box:
+    // If last_opened_box == bomb:
+    // explodeBomb
+    // end game
+    // winner
+    // else: (dus geen bom)
+    // change last_opened_box into opened box (grijs):
+    // interface veranderen --> JIJ BENT AAN DE BEURT YAY
+    // box = $('img').on('click', function() {
+    // krijg het id
+    // openBox(id)
+    // check of het nog gesloten, zo niet doe je niks
+    // open box --> return of het normale box is, hint of bom
+    // change properties of box (close == false)
+    // maak box grijs
+    // )}
+    // if box = bom:
+    // explodeBomb(loser)
+    // end game
+    // loser
+    // else if box = hint:
+    // show hint
+    // changeTurn() --> which will make yourTurn false
+    // call php function --> endgame.php
+    // if box = hint:
+    // pop-up met de hint
+    // aanpassen in json dat het niet meer jouw beurt is
+    // })
+    // else (bom niet intact --> jij hebt gewonnen! : )
+    // explodeBomb(winner)
+    // deletegame()
+
+
+    // } else {
+    // Niet jouw turn
+    // interface veranderen --> JIJ BENT NIET AAN DE BEURT NAY
+    // }
+    // }
 });
