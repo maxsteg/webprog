dc083dc184821b85b4823a3d5dabc006f0ced6d7
KarloSlot <k.h.r.slot@student.rug.nl>
2020-06-13 18:09:25 +0200

    Modify game.js, add crossmark.svg

--- scripts/game.js
+++ scripts/game.js
@@ -41,12 +41,29 @@ function openBox(box) {
     });
 }
 
-function explodeBomb(box, loser) {
-        $.post("endgame.php", {gamenumber: gameNumber, playernumber: playerNumber}, function(bomb) {
-            // background a color (circle animation --> in makegame.php even toevoegen aan het algemene spel object (e.g. bomblocation), in welk pakketje bom zit, zodat dit makkelijk te returnen is hier bij endgame)
-            // delete alle images
-            // slidein You are the winner/loser/other player left (status === "left")
-                // slideIn buttons (home button etc)
+function explodeBomb(xcoor, ycoor, status) {
+    $.post("endgame.php", {gamenumber: gameNumber, playernumber: playerNumber}, function(bomb) {
+        var x = xcoor - 5;     // Get the horizontal coordinate
+        var y = ycoor - 5;     // Get the vertical coordinate
+        document.body.innerHTML += '<div id="explosion" style="position:absolute;left:' + x + 'px;top:' + y + 'px;transition: border-radius 0.3s ease-in 0.2s, left ease-in 0.5s, top ease-in 0.5s, width ease-in 0.5s, height ease-in 0.5s;width:10px;height:10px;border-radius:50%;z-index:100;background:#f4a259;"></div>';
+        $('#explosion').css({'width': '100vw', 'height': '100vh', 'top': 0, 'left': 0,'border-radius': 0})
+        setTimeout(function() {
+            $('#explosion').css('transition', 'none');
+            $('.mx-auto.px-5').remove();
+            $('#explosion').append('<div id="endMessage" style="display:none;"></div>')
+            if (status === "left") {
+                $('#endMessage').append('<h1><b>Somebody left...</b></h1>');
+            } else {
+                $('#endMessage').append('<h1><b>The bomb has exploded!</b></h1>')
+                if (status === "winner") {
+                    $('#endMessage').append('<h3><b>Congratulations!</b> You are the winner!</h3>')
+                } else if (status === "loser") {
+                    $('#endMessage').append('<h3><b>Too bad!</b> You are the loser!</h3>')
+                }
+            }
+            $('#endMessage').append('<h5>Click the button to return to the home page.</h5><a href="index.php" class="btn homeButton"><img alt="Home Icon" src="images/home_icon.svg"></a>');
+            $('#endMessage').fadeIn(3000);
+            }, 500);
     });
 }
 
@@ -99,6 +116,32 @@ function game() {
 }
 
 $(function() {
+    $('.boxes img').on('click', function(e) {
+        var x = e.clientX - 5;     // Get the horizontal coordinate
+        var y = e.clientY - 5;     // Get the vertical coordinate
+        var status = "loser";
+        document.body.innerHTML += '<div id="explosion" style="position:absolute;left:' + x + 'px;top:' + y + 'px;transition: border-radius 0.3s ease-in 0.2s, left ease-in 0.5s, top ease-in 0.5s, width ease-in 0.5s, height ease-in 0.5s;width:10px;height:10px;border-radius:50%;z-index:100;background:#f4a259;"></div>';
+        $('#explosion').css({'width': '100vw', 'height': '100vh', 'top': 0, 'left': 0,'border-radius': 0})
+        setTimeout(function() {
+            $('#explosion').css('transition', 'none');
+            $('.mx-auto.px-5').remove();
+            $('#explosion').append('<div id="endMessage" style="display:none;"></div>')
+            if (status === "left") {
+                $('#endMessage').append('<h1><b>Somebody left...</b></h1>');
+            } else {
+                $('#endMessage').append('<h1><b>The bomb has exploded!</b></h1>')
+                if (status === "winner") {
+                    $('#endMessage').append('<h3><b>Congratulations!</b> You are the winner!</h3>')
+                } else if (status === "loser") {
+                    $('#endMessage').append('<h3><b>Too bad!</b> You are the loser!</h3>')
+                }
+            }
+            $('#endMessage').append('<h5>Click the button to return to the home page.</h5><a href="index.php" class="btn homeButton"><img alt="Home Icon" src="images/home_icon.svg"></a>');
+            $('#endMessage').fadeIn(3000);
+
+        }, 500);
+    });
+
     // Check if there is a second player
     let id = window.setInterval( function () {
         $.post("scripts/checksecondplayer.php", {gamenumber: gameNumber}, function(data) {
