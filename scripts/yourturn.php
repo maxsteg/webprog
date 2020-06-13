43c3fa675875658bb9c80fa7379a1dcd489151e5
riemkruit <r.k.ruitenbeek@student.rug.nl>
2020-06-13 20:20:30 +0200

    Modify to fix mistake

--- scripts/yourturn.php
+++ scripts/yourturn.php
@@ -3,7 +3,6 @@ $json_file = file_get_contents("../data/games.json");
 $games = json_decode($json_file, true);
 $gamenumber = $_POST['gamenumber'];
 $playernumber = $_POST['playernumber'];
-echo "true";
 if ($games[$gamenumber]['turn'] == $playernumber) {
     echo "true";
 } else {
