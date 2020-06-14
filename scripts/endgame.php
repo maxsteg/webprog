1edaa43455a7fada06de302021982d0757cfcf19
rina-d <t.r.donker@student.rug.nl>
2020-06-14 22:11:11 +0200

    Modify endgame.php

--- scripts/endgame.php
+++ scripts/endgame.php
@@ -3,16 +3,15 @@
 $json_file = file_get_contents("../data/games.json");
 $games = json_decode($json_file, true);
 $gamenumber = $_POST['gamenumber'];
-$bomb_active = $_POST['bombactive'];
 
-if (isset($gamenumber) && isset($bomb_active)) {
-    //if ($bomb_active == true):
-        // $bomb_active = false;
-    // else:
-        // delete game
-        // unset($games[$gamenumber]);
+if (isset($gamenumber)) {
+    $bomb_active = $games[$gamenumber]['bomb_active'];
+    if ($bomb_active == true) {
+        $bomb_active = false;
+    } else {
+        unset($games[$gamenumber]);
+    }
 
-    // Save to external file
     $json_file = fopen('../data/games.json', 'w');
     fwrite($json_file, json_encode($games));
     fclose($json_file);
